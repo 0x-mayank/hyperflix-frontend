e6b70b2dbb0f8c018b41c0db058c71565e498b4c
0x-mayank <mayankm1946@gmail.com>
2026-06-03 03:12:51 +0530

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 import App from "./App";
 import "./index.css";
@@ -11,5 +12,6 @@ ReactDOM.createRoot(
 ).render(
   <BrowserRouter>
     <App />
+    <Analytics />
   </BrowserRouter>
 );
