fd20126333558606a1ca12869c7dda1df3f6c300
0x-mayank <mayankm1946@gmail.com>
2026-06-03 18:07:30 +0530

    fix

--- src/api/streamApi.js
+++ src/api/streamApi.js
@@ -3,11 +3,8 @@ import api from "./axios";
 export const getMovieStream = (id) =>
   api.get(`/stream/movie/${id}`);
 
-export const getTvStream = (
-  id,
-  season,
-  episode
-) =>
-  api.get(
-    `/stream/tv/${id}/${season}/${episode}`
-  );
+export const getTvStream = (id, season, episode) =>
+  api.get(`/stream/tv/${id}/${season}/${episode}`);
+
+export const getAnimeStream = (id, season, episode) =>
+  api.get(`/anime/${id}/stream`, { params: { season, episode } });
