f9ea40c892c1c04b86acfb074150dba0d8a968c9
0x-mayank <mayankm1946@gmail.com>
2026-06-03 13:16:01 +0530

    speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import App from "./App";
 import "./index.css";
@@ -13,5 +14,6 @@ ReactDOM.createRoot(
   <BrowserRouter>
     <App />
     <Analytics />
+    < SpeedInsights/>
   </BrowserRouter>
 );
